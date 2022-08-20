0a4de5404af9973843bfc1118055885faec0c60c
RubsRz <rodriguezperezrubenalejandro@gmail.com>
2022-08-20 18:46:37 -0500

    finalizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GastosComponent } from './Components/gastos/gastos.component';
+import { IngresarPresupuestoComponent } from './Components/ingresar-presupuesto/ingresar-presupuesto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'ingresarPresupuesto', component: IngresarPresupuestoComponent},
+  { path: 'gastos', component: GastosComponent},
+  { path: '', redirectTo: '/ingresarPresupuesto', pathMatch:'full'},
+  { path: '**', redirectTo: '/ingresarPresupuesto', pathMatch:'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GastosComponent } from './Components/gastos/gastos.component';
+import { IngresarPresupuestoComponent } from './Components/ingresar-presupuesto/ingresar-presupuesto.component';
+import { IngresarGastoComponent } from './Components/gastos/ingresar-gasto/ingresar-gasto.component';
+import { ListarGastoComponent } from './Components/gastos/listar-gasto/listar-gasto.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GastosComponent,
+    IngresarPresupuestoComponent,
+    IngresarGastoComponent,
+    ListarGastoComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
